4bcefbad76650b348b7af4ff298a6ef3a465f4be
juanjpl <juan_jpl@hotmail.com>
2025-06-03 14:41:12 -0300

    commit 003

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 //console.log("Hola mundo");
+//prompt("Ingrese su nombre:")
 //const pi = 3.1416;
 //console.log(pi);
 
-const fs=require()
+const fs = require("fs");
+//console.log(fs);
+
+let mensaje ="Dentro de cada hombre existe una lucha entre el bien y el mal que no se resuelve"
+
+fs.writeFile("./archivo.txt", "Estoy creando un archivo con NODE.", ()=>{
+    try {
+        console.log("Exito")
+    } catch (error) {
+        console.log(error)
+    }
+})
