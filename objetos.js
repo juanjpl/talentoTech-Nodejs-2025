5d2a91433dc8e68716b074cb34301a58af4b25b5
juanjpl <juan_jpl@hotmail.com>
2025-06-04 14:25:31 -0300

    commit 002

--- objetos.js
+++ objetos.js
@@ -1,1 +1,34 @@
-//objetos
+//objetos literales {}
+
+const personaje = {
+    nombre:"Homero",
+    apellido:"Simpson",
+    casado:true,
+    edad:38
+}
+
+console.log(personaje)
+console.log(personaje.nombre)
+console.log(personaje.edad)
+console.log(personaje.apellido)
+console.log(personaje.casado)
+
+console.log(personaje["casado"])
+console.log(personaje["apellido"])
+
+const personaje2 = {
+    nombre:"Ned",
+    apellido:"Flanders",
+    casado:true,
+    edad:60,
+
+    diLoTuyo: function(){
+        console.log("Hola tarola, soy Ned flanders")
+    }
+}
+console.log(personaje2)
+
+personaje2.vecino=personaje ;
+console.log(personaje2)
+
+personaje2.diLoTuyo();
