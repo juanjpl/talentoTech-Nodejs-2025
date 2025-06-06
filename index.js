181b4575b2e5a726f09dc550bdd3672f4841d1d2
juanjpl <juan_jpl@hotmail.com>
2025-06-06 12:58:54 -0300

    commit 002

--- index.js
+++ index.js
@@ -11,13 +11,45 @@ fetch("https://jsonplaceholder.typicode.com/posts")
 
 
 //Axios --- compatibles con codigo antiguo
+const axios = require("axios");
+/*
+axios.get("https://jsonplaceholder.typicode.com/posts")
+.then(response=>{
+    console.log(response.data);
+})
+    */
+
+//GET   -   Hace un pedido
 axios.get("https://jsonplaceholder.typicode.com/posts")
 .then(response=>{
     console.log(response.data);
 })
 
-//GET   -   Hace un pedido
+axios.post("https://jsonplaceholder.typicode.com/posts")
+.then(response=>{
+    console.log(response.data);
+})
+
+
 //POST  -   Crea un recurso
+const config ={
+    method: "POST",
+    headers:{
+        "Content-Type":"application/json"
+    },
+    body: JSON.stringify({
+        title:"Nuevo Post",
+        body:"Soy un nuevo post",
+        userId:100,
+        id:200
+    })
+}
+fetch("https://jsonplaceholder.typicode.com/posts", config)
+.then( (response) => response.json())
+.then((data)=>{
+    console.log(data);
+})
+
 //PUT   -   Actualizar un recurso
 //PATCH -   Actualizar un algo de un recurso
 //DELETE-   Elimina un recurso
