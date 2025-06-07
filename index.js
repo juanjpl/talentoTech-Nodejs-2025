4c15ba7761bf776c1907a262b00ed619fc27e732
Juan Lima <juan_jpl@hotmail.com>
2025-06-07 00:48:02 -0300

    commit 003

--- index.js
+++ index.js
@@ -2,26 +2,31 @@
 
 import { getProducts } from "./modules/getProducts.js";
 import { getProductsID } from "./modules/getProductsID.js";
+import { deleteProductID } from "./modules/deleteProductID.js";
 
 //console.log(process.argv); // Imprimirá el array de argumentos
 //console.log(process.argv[0])
 //console.log(process.argv[1])
 //console.log(process.argv[2])
 //console.log(process.argv[3])
-const parametro1 = process.argv[2];
+const peticion = process.argv[2];
 const parametro2 = process.argv[3];
 const parametro3 = process.argv[4];
 const parametro4 = process.argv[5];
 
 //getProducts();
 
-if (parametro1 === "GET" && parametro2==="products"){
+if (peticion === "GET" && parametro2==="products"){
     getProducts();
-}else if (parametro1 === "GET" && parametro2.length >9) {
+}else if (peticion === "GET" && parametro2.length >9) {
+    if(parametro2.slice(0,8)==="products"){
     //console.log(parametro2)
     //console.log(parametro2.slice(9));
+    //console.log(parametro2.slice(0,8))
     getProductsID(parametro2.slice(9));
-}else if(parametro1 === "DELETE" && parametro2.length >9){
+    }
+    
+}else if(peticion === "DELETE" && parametro2.length >9){
      deleteProductID(parametro2.slice(9));
 }else{
   console.log("Error! Parametros ingresados incorrectamente.")
