a636a1fbeeab1d9e7fc12c3108f53353751b5b7e
juanjpl <juan_jpl@hotmail.com>
2025-07-03 16:00:30 -0300

    001

--- express-api/src/controllers/users.controller.js
+++ express-api/src/controllers/users.controller.js
@@ -6,7 +6,7 @@ const getAllUsers =(req,res)=>{
     res.status(200).json(users)
 };
 
-<<<<<<< HEAD
+
 const getUserById=(req, res )=>{
     const {id} = req.params;
 
@@ -76,64 +76,6 @@ const updateUser = (req,res)=>{
 }
 
 
-=======
-const createUser = (req,res)=>{
-    try {
-          const {name,email} = req.body
-    const newUser = userService.createUser({name, email})
-    res.status(201).json(newUser)
-    } catch (error) {
-        res
-        .status(500)
-        .json({message:"Error interno del servidor", error:error.message})
-    }
-  
-}
 
-const updateUser = (req,res)=>{
-    const {id} =req.params;
-    const {name} = req.body;
-    try {
-        if(!id){
-            return res
-            .status(400)
-            .json({message:"El ID es requerido"})
-        }
-        const user = userService.updateUser(id, name);
-
-        if(!user){
-            return res.status(404).json({message:"Usuario no encontrado"})
-        }
-        res
-        .status(200)
-        .json({message:"Usuario actualizado con exito!", user:user})
-        
-    } catch (error) {
-         res
-        .status(500)
-        .json({message:"Error interno del servidor", error:error.message})
-    }
-
-  
-
-}
-  const getUserById = (req,res)=>{
-        const {id} =req.params;
-        try {
-             const user = userService.getUserById(id);
-
-        if(!user){
-            return res.status(404).json({message:"Usuario no encontrado"})
-        }
-        res
-        .status(200).json({message:"Usuario encontrado!", payload:user})
-            
-        } catch (error) {
-             res
-        .status(500)
-        .json({message:"Error interno del servidor", error:error.message})
-        }
-    }
->>>>>>> f1bde4183a41ab8bf1f6a4c3efe41ae012cd00cb
 
 export default {getAllUsers,createUser,updateUser, getUserById}
--- express-api/src/routes/users.routes.js
+++ express-api/src/routes/users.routes.js
@@ -13,15 +13,11 @@ router.get("/:",userController.getUserById);
 //Post
 router.post("/",userController.createUser);
 
-<<<<<<< HEAD
 //Put
 router.put("/:id",userController.updateUser);
 
 //Post
 router.post("/",userController.createUser);
 
-=======
-router.put("/:id",userController.updateUser);
 
->>>>>>> f1bde4183a41ab8bf1f6a4c3efe41ae012cd00cb
 export default router;
