ce2ab8876d83e3bcbb3fb7f029cfc88e32db32ea
Juan Lima <juan_jpl@hotmail.com>
2025-07-04 20:00:25 -0300

    fix 001

--- express-api/src/services/users.services.js
+++ express-api/src/services/users.services.js
@@ -17,20 +17,6 @@ const createUser = (user)=>{
     return newUser
 }
 
-<<<<<<< HEAD
-const updateUser = (id, name)=>{
-   const userFound = getUserById(id);
-   if(!userFound){
-    return null;
-   }
-   userFound.name=name;
-   return userFound;
-}
-
-
-
-export default {getAllUsers , createUser, updateUser,getUserById};
-=======
 const updateUser=(id, name)=>{
     const userFound = getUserById(id);
     if(!userFound)return null
@@ -38,10 +24,4 @@ const updateUser=(id, name)=>{
     return userFound;
 }
 
-const getUserById= (id)=>{
-    return users.find((user)=>user.id === id);
-}
-
-
 export default {getAllUsers , createUser, updateUser , getUserById};
->>>>>>> f1bde4183a41ab8bf1f6a4c3efe41ae012cd00cb
