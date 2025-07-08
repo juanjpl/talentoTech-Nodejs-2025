29de725ff543c430319c6e81100f78e5d866aa27
Juan Lima <juan_jpl@hotmail.com>
2025-07-08 20:30:56 -0300

    completing project 001

--- clase-015/src/index.js
+++ clase-015/src/index.js
@@ -2,24 +2,36 @@ import express from "express";
 import { join, __dirname } from "./utils/index.js";
 import userRoutes from "./routes/user.route.js";
 import productRoutes from "./routes/product.route.js";
+import authRouter from './src/routes/auth.routes.js';
+import bodyParser from "body-parser";
+import { authentication } from "./middlewares/auth.middleware.js";
+
+app.use(bodyParser.json());
+
+//Autenticacion de forma global
+//app.use(authentication);
+
 //import {db} from "./data/data.js";
 
 const app = express();
 app.set("PORT", 3000);
 
-// middlewares
+// Middlewares
 
 app.use(express.json());
 app.use(express.static(join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
-//routes
+// Routes
+
+app.use('/auth', authRouter);
 app.get("/", (req, res) => {
   res.json({ title: "Home Page" });
 });
 
-app.use("/api/users", userRoutes);
-app.use("/api/products", productRoutes);
+app.use("/api/users",authentication, userRoutes);
+app.use("/api/products",authentication, productRoutes);
+app.use("/api/categories",authentication, productRoutes);
 
 //listeners
 app.listen(app.get("PORT"), () => {
--- clase-015/src/routes/product.route.js
+++ clase-015/src/routes/product.route.js
@@ -1,12 +1,16 @@
 //router
 import { Router } from "express";
 import productController from '../controllers/product.controller.js'
+import { authentication } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
 
 router.get('/', productController.getProducts);
-
+router.get('/:id',authentication, productController.getProducts);
+/*
+router.post('/',authentication, productController.createProducts);
+*/
 
 
 
