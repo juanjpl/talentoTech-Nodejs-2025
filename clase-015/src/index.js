a751a364d577458463b223e7f9dfc4e477f6a2cf
juanjpl <juan_jpl@hotmail.com>
2025-07-17 19:30:22 -0300

    fix readme

--- clase-015/src/index.js
+++ clase-015/src/index.js
@@ -5,38 +5,27 @@ import userRoutes from "./routes/user.route.js";
 import productRoutes from "./routes/product.route.js";
 import authRouter from './routes/auth.routes.js';
 
-import { authentication } from "./middlewares/auth.middleware.js";
-
-
-
-//Autenticacion de forma global
-//app.use(authentication);
-
-//import {db} from "./data/data.js";
 
 const app = express();
 app.set("PORT", 3000);
 
 // Middlewares
-
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.static(join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
+
 // Routes
-
-   
-
 app.use('/auth', authRouter);
 
 app.get("/", (req, res) => {
   res.json({ title: "Home Page" });
 });
 
-app.use("/api/users",authentication, userRoutes);
+app.use("/api/users", userRoutes);
 app.use("/api/products",productRoutes);
-app.use("/api/categories",authentication, productRoutes);
+app.use("/api/categories", productRoutes);
 
 
 
